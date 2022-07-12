c9055a84289658547317b13c46e3748ecc7775d6
mikhail <mn.02@mail.ru>
2022-07-12 21:23:57 +0300

    9 неделя / Соберем сниппеты с Хабрахабр

--- webapp/news/parsers/habr.py
+++ webapp/news/parsers/habr.py
@@ -1,21 +1,33 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import locale
+import platform
 
 from bs4 import BeautifulSoup
 
 from webapp.news.parsers.utils import get_html, save_news
 
-# def get_news_snippets():
-#     html = get_html("https://habr.com/ru/search/?target_type=posts&q=python&order_by=date")
-#     if html:
-#         soup = BeautifulSoup(html, 'html.parser')
-#         all_news = soup.find('ul', class_='content-list_posts').findAll('li', class_='content-list__item_post')
-#         for news in all_news:
-#             title = news.find('a', class_='post__title_link').text
-#             url = news.find('a', class_='post__title_link')['href']
-#             published = news.find('span', class_='post__time').text
-#             print(title, url, published)
 
 
+if platform.system() == 'Windows':
+    locale.setlocale(locale.LC_ALL, "russian")
+else:
+    locale.setlocale(locale.LC_TIME, 'ru_RU')
+
+
+def parse_habr_date(date_str):
+    while '  ' in date_str:
+        date_str = date_str.replace('  ', ' ')
+    if 'сегодня' in date_str:
+        today = datetime.now()
+        date_str = date_str.replace('сегодня', today.strftime('%d %B %Y'))
+    elif 'вчера' in date_str:
+        yesterday = datetime.now() - timedelta(days=1)
+        date_str = date_str.replace('вчера', yesterday.strftime('%d %B %Y'))
+    try:
+        return datetime.strptime(date_str, '%d %B %Y в %H:%M')
+    except ValueError:
+        return datetime.now()
+        
 
 def get_news_snippets():
     html = get_html("https://habr.com/ru/search/?target_type=posts&q=python&order_by=date")
@@ -24,6 +36,8 @@ def get_news_snippets():
         all_news = soup.find('div', class_='tm-articles-list').findAll('article', class_='tm-articles-list__item')
         for news in all_news:
             title = news.find('a', class_='tm-article-snippet__title-link').text
-            url = news.find('a', class_='tm-article-snippet__title-link')['href']
+            url = 'https://habr.com' + news.find('a', class_='tm-article-snippet__title-link')['href']
             published = news.find('span', class_='tm-article-snippet__datetime-published').text
+            
+            published = parse_habr_date(published)
             print(title, url, published)
